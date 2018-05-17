753ef062b91fe48c9f1b5fd4dc0d2e713243af92
Minh-Quan Nguyen <Minh-Quan_Nguyen@neimanmarcus.com>
2018-05-17 12:31:52 -0500

    Added and Modified | functions.js, index.html

---
+++ section-2/functions.js
@@ -0,0 +1,11 @@
+function calculateAge(yearOfBirth) {
+    var currentYear = 2018;
+    return currentYear - yearOfBirth; 
+}
+
+function yearsUntilRetirement(personName, yearOfBirth) {
+    var personAge = calculateAge(yearOfBirth);
+    return 65 - personAge;
+}
+
+console.log(yearsUntilRetirement("B3LL", 1995));
