fdf2cdbedc707b64392b094db0ee6aa466fbbaae
Minh-Quan Nguyen <Minh-Quan_Nguyen@neimanmarcus.com>
2018-05-21 15:31:06 -0500

    Finished | app.js, Dice Game Done

--- section-4/app.js
+++ section-4/app.js
@@ -13,15 +13,122 @@
 /**
  * Lecture : DOM Manipulation
  * 
- * DOM - Document Object Model. It is a structured representation of an HTML document. DOMs are used t
- * connect webpages to scripts like JS.
+ * DOM - Document Object Model. It is a structured representation of an HTML document. 
+ * DOMs are used to connect webpages to scripts like JS.
  * 
  * For each HTML Box, there is an object in the DOM that we can access and interact with.
  */
 
 // ------------------------------ Global Variables Initialization ------------------------------ //
-var playerScores, roundCount, currentPlayer;
-playerScores = [0, 0];
-roundCount = 0;
-currentPlayer = 0;
+var playerScores, roundScore, currentPlayer, gamePlaying;
 
+// --- Start a New Game
+init();
+
+//  --- Enable "Roll Dice" Button
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    if (gamePlaying) {
+        // 1. Random Number
+        var diceValue = Math.floor(Math.random() * 6) + 1;
+
+        // 2. Display Results
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + diceValue + '.png';
+
+
+        // 3. Update the Current Round IF the rolled number was NOT a 1
+        if (diceValue !== 1) {
+            // Add Score
+            roundScore += diceValue;
+            document.querySelector('#current-' + currentPlayer).textContent = roundScore;
+        } else {
+            // Next Player Turn
+            nextPlayer();
+        }
+    }
+});
+
+// --- Enable "Hold" Button
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    if (gamePlaying) {
+        // Add current score to player global score
+        playerScores[currentPlayer] += roundScore;
+
+        // Update the UI
+        document.querySelector('#score-' + currentPlayer).textContent = playerScores[currentPlayer];
+
+        // Check if player won the game
+        if (playerScores[currentPlayer] >= 100) {
+            document.querySelector('#name-' + currentPlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + currentPlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + currentPlayer + '-panel').classList.remove('active');
+
+            gamePlaying = false;
+        } else {
+            // Next Player Turn
+            nextPlayer();
+        }
+    }
+});
+
+// --- Enable "New Game" Button
+document.querySelector('.btn-new').addEventListener('click', init());
+
+/**
+ * init()
+ * 
+ * This Function will initialize a new game.
+ * This will reset all scores and player turns.
+ * 
+ * @param none
+ */
+function init() {
+    playerScores = [0, 0];
+    roundScore = 0;
+    currentPlayer = 0;
+    gamePlaying = true;
+
+    // DOM Manipulation
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
+
+/**
+ * nextPlayer()
+ * 
+ * This function handles the changing of players.
+ * 
+ * @param none
+ */
+function nextPlayer() {
+    // Next Player Turn
+    currentPlayer === 0 ? currentPlayer = 1 : currentPlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    // Adding and Removing Classes in HTML
+    // Toggle is better than add() and remove()
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
