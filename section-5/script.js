2fb53780c0d5568dc82fca03473b80c7a19b20a2
Minh-Quan Nguyen <Minh-Quan_Nguyen@neimanmarcus.com>
2018-05-21 17:05:59 -0500

    Added | Section 5 Folder and Files

---
+++ section-5/script.js
@@ -0,0 +1,12 @@
+/**
+ * Section 5 Lecture Notes
+ * 
+ *  - Everything is an object... (Almost Everything). This is one of the unique and fundamental
+ *  things about JS compared to other languages.
+ *  - Object Oriented Programming : Objects interacting with one anotehr through methods and properties.
+ *  Additionally, used to store data, structures, etc.
+ *  - Constructor : Acts as a blueprint and creates instances of objects
+ *  - Inheritance : When one object is based on another object.
+ *  - JS is a prototype based language. Each and every JS Object, has a Prototype property.
+ *  Therefore, every JS Object has a prototype property, making inheritance possible.
+ */
