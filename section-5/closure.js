8a911284a29e2a7a92b5f648fa471367343e8ce0
Minh-Quan Nguyen <Minh-Quan_Nguyen@neimanmarcus.com>
2018-05-23 20:42:55 -0500

    Modified | closure.js, Finished the closure section of lecture

--- section-5/closure.js
+++ section-5/closure.js
@@ -10,5 +10,31 @@ function retirementFunction(retirementAge) {
 }
 
 var retirementUS = retirementFunction(65);
+var retirementGermany = retirementFunction(65);
+var retirementIceland = retirementFunction(67);
+
 retirementUS(1995);
-retirementFunction(65)(1995);
+retirementGermany(1995);
+retirementIceland(1995);
+
+// Alternative way to write the code above
+// retirementFunction(65)(1995);
+
+function interviewQuestions(someJob) {
+    return function(someName) {
+        if (someJob === 'engineer') {
+            console.log(someName + ', do you know what closure is?');
+        }
+        else if (someJob === 'professor') {
+            console.log(someName + ', what courses do you teach?');
+        } else {
+            console.log('Hello ' + someName + ', what do you do for a living?');
+        }
+    }
+}
+
+// NOTES: Explanation of Code below
+// interviewQuestions('professor') : This will return the function in lines 24-33
+// ('Maria') : Is passed in function('someName') in Line 24
+interviewQuestions('professor')('Maria');
+
